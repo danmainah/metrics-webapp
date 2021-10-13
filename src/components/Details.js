a9725046035e652348ba283662dc8e0de1ba475d
unknown <danielmainawaweru@gmail.com>
2021-10-13 15:54:53 +0300

    Add styling to details and home components

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,19 +1,50 @@
 import { Link } from 'react-router-dom';
 
-const Details = (corona) => (
-  <>
-    <h2>
-      This is the details page of
-      {corona.corona.country}
-    </h2>
-    <h3>
-      Number of Cases registered today
-      { corona.corona.todayCases }
-    </h3>
-    <Link to="/">
-      Home
-    </Link>
-  </>
-);
+const Details = (corona) => {
+  const {
+    country, todayCases, countryInfo, tests,
+  } = corona.corona;
+  return (
+    <>
+      <div className="card mb-3">
+        <Link to="/">
+          <i className="fa fa-arrow-left m-1" />
+          {' '}
+          Go Back
+        </Link>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={countryInfo.flag} className="img-fluid rounded-start p-1 w-100" alt={country} />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">
+                {' '}
+                {country}
+                {' '}
+                corona informatin
+              </h5>
+              <p className="card-text">
+                {' '}
+                Number of Cases registered today
+                {' '}
+                {todayCases }
+              </p>
+              <p className="card-text">
+                <small className="text">
+                  Number of tests done
+                  {' '}
+                  {' '}
+                  {tests}
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
 
 export default Details;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,28 +4,56 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const state = useSelector((state) => state.corona);
   const world = useSelector((state) => state.world);
+
+  console.log(state);
+  const data = state.filter((value) => value.todayCases)
+    .sort((a, b) => b.todayCases - a.todayCases)
+    .splice(0, 10);
+  console.log(data);
   return (
     <>
-      <ul>
-        <h2>
-          Population with Covid per 1 million:
-          {world.activePerOneMillion}
-        </h2>
-        <h3>
-          Countries affected:
-          {world.affectedCountries}
-        </h3>
-      </ul>
-      <ul>
-        {state.map((corona) => (
-          <li key={corona.country}>
-            <Link to={`/${corona.country}`}>
-              {corona.country}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <h2>Word Corona Statistics</h2>
+            <ul>
+              <h2>
+                Population with Covid per 1 million:
+                {world.activePerOneMillion}
+              </h2>
+              <h3>
+                Countries affected:
+                {world.affectedCountries}
+              </h3>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          {data.map((corona) => (
+            <Link className="card col-6 col-lg-4 text-decoration-none text-white" to={`/${corona.country}`} key={corona.country}>
+
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={corona.countryInfo.flag} className="img-fluid rounded-start p-1 w-100" alt={corona.country} />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+
+                    {corona.country}
+
+                    <p className="card-text">
+                      Today corona cases:
+                      {corona.todayCases}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
             </Link>
-            {corona.todayRecovered}
-          </li>
-        ))}
-      </ul>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
