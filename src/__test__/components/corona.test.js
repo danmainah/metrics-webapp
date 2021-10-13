18dc81c3aafb5cb32ed44c9e70ea43dd08ed14f3
unknown <danielmainawaweru@gmail.com>
2021-10-13 18:10:21 +0300

    Add test to details component

--- src/__test__/components/corona.test.js
+++ src/__test__/components/corona.test.js
@@ -3,9 +3,8 @@ import { screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import store from '../redux/store';
-import Home from '../components/Home';
-import Details from '../components/Details';
+import store from '../../redux/store';
+import Home from '../../components/Home';
 
 test('Check if Home is rendering correctly', () => {
   const body = document.createElement('tbody');
--- src/__test__/components/details.test.js
+++ src/__test__/components/details.test.js
@@ -3,22 +3,8 @@ import { screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import store from '../redux/store';
-import Home from '../components/Home';
-import Details from '../components/Details';
-
-test('Check if Home is rendering correctly', () => {
-  const body = document.createElement('tbody');
-  ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
-    </Provider>,
-    body,
-  );
-  expect(screen).toMatchSnapshot();
-});
+import store from '../../redux/store';
+import Details from '../../components/Details';
 
 test('Check if Details is rendering correctly', () => {
   const body = document.createElement('tbody');
